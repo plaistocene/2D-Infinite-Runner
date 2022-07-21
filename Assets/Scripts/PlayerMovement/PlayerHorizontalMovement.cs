a772372f9f47b08736a9dd9dc2998955032b423d
I. Bugra Aslan <46376529+plaistocene@users.noreply.github.com>
2022-07-21 15:13:14 +0300

    Added character speed recover for recovering from jump
    When character recovers from jump it's speed getting lowered due to force applied gets 0, so getting character's speed from 0.7 seconds before and applying that speed to character if character speed gets lowered than 0.7 second before approach used

--- Assets/Scripts/PlayerMovement/PlayerHorizontalMovement.cs
+++ Assets/Scripts/PlayerMovement/PlayerHorizontalMovement.cs
@@ -7,12 +7,17 @@ public class PlayerHorizontalMovement : MonoBehaviour
 
     public float distance = 0f;
     
-    public float forwardForce = 2f;
-    public float maxHorizontalVelocity = 100f;
+    public float forwardForce = 500f;
+    public float maxHorizontalVelocity = 150f;
+    // This velocity will be used if player can recover from hitting the side of the building but manage to jump in time
+    public float velocityFromPast = 0f;
+    public float timeForPast = 0.7f;
+    public float pastTimeCounter = 0f;
+    private bool _speedRecovered = true;
 
     public bool canReverseDash = true;
     public bool reverseDash = false;
-    [Range(1, 10)] public float reverseDashForce = 2f;
+    [Range(1, 10)] public float reverseDashForce = 3f;
     public float reverseDashCooldownTime = 4f;
     public float reverseDashTimer = 0f;
 
@@ -49,13 +54,35 @@ public class PlayerHorizontalMovement : MonoBehaviour
         }
     }
 
+    // TODO: This parts needs a refactoring
     void FixedUpdate()
     {
         // Horizontal movement
         if (_rb2d.velocity.x <= maxHorizontalVelocity)
         {
             _rb2d.AddForce(new Vector2(forwardForce, 0) * Time.fixedDeltaTime);
+            // _rb2d.velocity += Vector2.right * (forwardForce * Time.fixedDeltaTime);
         }
+
+        pastTimeCounter += Time.fixedDeltaTime;
+        
+        if (timeForPast <= pastTimeCounter && _speedRecovered)
+        {
+            pastTimeCounter = 0f;
+            velocityFromPast = _rb2d.velocity.x;
+        }
+
+        if (_rb2d.velocity.x < velocityFromPast && !shouldSpeedRecover)
+        {
+            _speedRecovered = false;
+            _rb2d.velocity += Vector2.right * velocityFromPast;
+        }
+        
+        if(_rb2d.velocity.x >= velocityFromPast)
+        {
+            _speedRecovered = true;
+        }
+        
 
         // ReverseDash
         if (reverseDash)
