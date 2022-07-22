1382aefa0f618a0f297b1a5cfa8e74636d00ca15
I. Bugra Aslan <46376529+plaistocene@users.noreply.github.com>
2022-07-22 14:27:17 +0300

    Some unused parts cleaned from code

--- Assets/Scripts/PlayerMovement/PlayerHorizontalMovement.cs
+++ Assets/Scripts/PlayerMovement/PlayerHorizontalMovement.cs
@@ -1,20 +1,19 @@
-using System;
 using UnityEngine;
 
 public class PlayerHorizontalMovement : MonoBehaviour
 {
     private Rigidbody2D _rb2d;
 
-    public float distance = 0f;
+    public float distance;
 
     public float forwardForce = 500f;
 
     public float maxHorizontalVelocity = 150f;
 
     // This velocity will be used if player can recover from hitting the side of the building but manage to jump in time
-    public float velocityFromPast = 0f;
+    public float velocityFromPast;
     public float timeForPast = 2f;
-    public float pastTimeCounter = 0f;
+    public float pastTimeCounter;
     private bool _speedRecovered = true;
     
     
