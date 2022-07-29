7e89c71067ec40dda86e8bcd1a26d82974081183
I. Bugra Aslan <46376529+plaistocene@users.noreply.github.com>
2022-07-29 17:39:05 +0300

    Unused variables and Start() function cleared

--- Assets/Scripts/Player/PlayerMovement/PlayerHorizontalMovement.cs
+++ Assets/Scripts/Player/PlayerMovement/PlayerHorizontalMovement.cs
@@ -6,36 +6,19 @@ public class PlayerHorizontalMovement : MonoBehaviour
 
     public float distance;
 
-    public float forwardForce = 500f;
     public float maxHorizontalVelocity;
-    public bool maxSpeedReached;
-
-    // This velocity will be used for if player can recover from hitting the side of the building but manage to jump in time
-    public float velocityFromPast;
-    public float timeForPast = 2f;
-    public float pastTimeCounter;
-    private bool _speedRecovered = true;
-
-    private VelocityChangeFunctions _velocityChangeFunctions;
-    private PlayerReverseDash _playerReverseDash;
-
+    
     public Vector2 velocity;
 
     private void Awake()
     {
-        _velocityChangeFunctions = GetComponent<VelocityChangeFunctions>();
         _rb2d = GetComponent<Rigidbody2D>();
     }
-
-    private void Start()
-    {
-        _playerReverseDash = GetComponent<PlayerReverseDash>();
-    }
-
+    
     void FixedUpdate()
     {
         // Horizontal movement
-        if (velocity.x <= 50)
+        if (_rb2d.velocity.x <= 50)
         {
             _rb2d.velocity = new Vector2(50, _rb2d.velocity.y);
         }
